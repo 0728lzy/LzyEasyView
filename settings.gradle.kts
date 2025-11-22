31201ecc34220c56c8e371e5afa308be3f5ce3fe
Daniel Liu <1276223841@qq.com>
2025-11-22 12:16:58 +0800

    调整镜像源

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,10 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/nexus/content/groups/public/")
-        maven("https://maven.aliyun.com/nexus/content/repositories/google")
+        // 如果不是在 JitPack 服务器上，则使用阿里云镜像加速
+        if (System.getenv("JITPACK") != "true") {
+            maven("https://maven.aliyun.com/nexus/content/groups/public/")
+            maven("https://maven.aliyun.com/nexus/content/repositories/google")
+        }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -11,10 +14,14 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/nexus/content/groups/public/")
-        maven("https://maven.aliyun.com/nexus/content/repositories/google")
+        // 如果不是在 JitPack 服务器上，则使用阿里云镜像加速
+        if (System.getenv("JITPACK") != "true") {
+            maven("https://maven.aliyun.com/nexus/content/groups/public/")
+            maven("https://maven.aliyun.com/nexus/content/repositories/google")
+        }
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
